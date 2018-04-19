98e589b42ebc036d54842de9c1d84aa7bbf4ed7c
cammiboi <camjnaw@shaw.ca>
2018-04-19 11:17:05 -0600

    Added space

--- include/jsonFunctions.h
+++ include/jsonFunctions.h
@@ -9,7 +9,7 @@
 
 int jsonTagFinder(char *jsonStringToSearch, char *tagToFind, char *stringToStoreValue, int maxValueLength);
 
-// saves the json array (must be of numbers) at a given tag in an interger array
+// saves the json array (must be of numbers) at a given tag in an interger array 
 
 int jsonGetArray(char *jsonStringToSearch, char *tagToFind, int *arrayToStoreValues, int maxArrayLength);
 
